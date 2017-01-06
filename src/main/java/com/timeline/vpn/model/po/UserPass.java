be9fa5d280f6f5d054968345cf082afcf59bc0e7
liguoqing <liguoqing@lianjia.com>
2017-01-06 22:03:18 +0800

    11

---
+++ src/main/java/com/timeline/vpn/model/po/UserPass.java
@@ -0,0 +1,28 @@
+package com.timeline.vpn.model.po;
+/**
+ * @author gqli
+ * @date 2017年1月6日 下午9:54:43
+ * @version V1.0
+ */
+public class UserPass {
+    private String name;
+    private String pass;
+    public UserPass(String name,String pass){
+        this.name = name;
+        this.pass = pass;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
+
---
+++ src/main/java/com/timeline/vpn/service/impl/UserAuthData.java
@@ -0,0 +1,27 @@
+package com.timeline.vpn.service.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.timeline.vpn.model.po.UserPass;
+
+/**
+ * @author gqli
+ * @date 2017年1月6日 下午9:54:23
+ * @version V1.0
+ */
+public class UserAuthData {
+    private static List<UserPass> data;
+    private static int index=0;
+    static {
+        data = Arrays.asList(new UserPass("usertest1", "usertest123"),
+                new UserPass("usertest2", "usertest123"), new UserPass("usertest3", "usertest123"),
+                new UserPass("usertest4", "usertest123"));
+    }
+    public static UserPass getOne(){
+        UserPass pass = data.get(index);
+        index = (index+1)%data.size();
+        return pass;
+    }
+}
+
