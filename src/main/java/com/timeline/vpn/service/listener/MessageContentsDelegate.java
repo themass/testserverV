fcdc7f39e803a328b7015f4635b2cfeb55c4ae99
liguoqing <liguoqing@lianjia.com>
2017-04-12 19:15:00 +0800

    11

---
+++ src/main/java/com/timeline/vpn/service/listener/MessageContentsDelegate.java
@@ -0,0 +1,11 @@
+package com.timeline.vpn.service.listener;
+/**
+ * @author gqli
+ * @date 2017年4月12日 下午3:15:28
+ * @version V1.0
+ */
+public interface MessageContentsDelegate {
+    void handleMessage(String text);
+    void handleMessage(Object text);
+}
+
