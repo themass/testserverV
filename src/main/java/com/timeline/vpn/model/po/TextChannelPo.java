9a9ee308e4360b4b53556c6ba8cb7aef4946ff92
liguoqing <liguoqing@lianjia.com>
2017-08-22 21:08:57 +0800

    11

---
+++ src/main/java/com/timeline/vpn/model/po/TextChannelPo.java
@@ -0,0 +1,41 @@
+package com.timeline.vpn.model.po;
+
+import java.util.Date;
+
+/**
+ * @author gqli
+ * @date 2017年8月21日 下午8:22:50
+ * @version V1.0
+ */
+public class TextChannelPo {
+    private Integer id;
+    private String name;
+    private String url;
+    private Date updateTime;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+    
+}
+
