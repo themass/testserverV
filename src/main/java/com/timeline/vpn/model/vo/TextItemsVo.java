a1bdd7f6231064b42ab3efd89655249137b2c38b
liguoqing <liguoqing@lianjia.com>
2017-08-22 21:10:02 +0800

    11

---
+++ src/main/java/com/timeline/vpn/model/vo/TextItemsVo.java
@@ -0,0 +1,39 @@
+package com.timeline.vpn.model.vo;
+
+/**
+ * @author gqli
+ * @date 2017年8月21日 下午8:22:50
+ * @version V1.0
+ */
+public class TextItemsVo {
+    private Integer id;
+    private String name;
+    private String channel;
+    private String fileDate;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getChannel() {
+        return channel;
+    }
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+    public String getFileDate() {
+        return fileDate;
+    }
+    public void setFileDate(String fileDate) {
+        this.fileDate = fileDate;
+    }
+   
+}
+
