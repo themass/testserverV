751da21fadc40867f426f66418f1c614e8b0a3a0
liguoqing <liguoqing@lianjia.com>
2017-08-31 15:21:01 +0800

    11

--- src/main/java/com/timeline/vpn/service/impl/CdnChooseUtil.java
+++ src/main/java/com/timeline/vpn/service/impl/CdnChooseUtil.java
@@ -1,5 +1,8 @@
 package com.timeline.vpn.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.timeline.vpn.Constant;
 import com.timeline.vpn.service.job.reload.FileIpCache;
 import com.timeline.vpn.service.job.reload.HostCheck;
@@ -11,19 +14,25 @@ import com.timeline.vpn.service.job.reload.ZhIpCache;
  * @version V1.0
  */
 public class CdnChooseUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CdnChooseUtil.class);
     public static String getImageBaseUrl(String ip){
         if(HostCheck.isMyHost(ip)){
+            LOGGER.info("cdn : ip={},vpn已连接",ip);
             return null;
         }
         if(ZhIpCache.isChinaIp(ip)){
+            LOGGER.info("cdn : ip={},中国ip",ip);
             return FileIpCache.getHost(Constant.FileIpTYPE.IMG, Constant.FileIpExtra.ZH);
         }
+        LOGGER.info("cdn : ip={},国外ip",ip);
         return FileIpCache.getHost(Constant.FileIpTYPE.IMG, Constant.FileIpExtra.OTHER);
     }
     public static String getFetchImageBaseUrl(String ip ,String url){
         if(HostCheck.isMyHost(ip)){
+            LOGGER.info("cdn : ip={},vpn已连接",ip);
             return null;
         }
+        LOGGER.info("cdn : ip={},没有连接vpn",ip);
         return url;
     }
     public static String getBookBaseUrl(String ip){
