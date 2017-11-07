907eeae0f0b1d1e7aa99fe2832ab94fdd642c5f7
liguoqing <liguoqing@lianjia.com>
2017-11-07 10:49:15 +0800

    11

--- src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
@@ -122,6 +122,9 @@ public class DataServiceImpl implements DataService {
         VersionInfoVo vo = VoBuilder.buildVo(versionDao.getLast(platform,channel), VersionInfoVo.class,null);
         vo.setAdsShow(false);
         vo.setLogUp(true);
+        if(baseQuery.getAppInfo().getVersion().equals("001000002009")){
+            return vo;
+        }
         if(!channel.equals("VPN")){
             AppVersion vpnVer = versionDao.getLast(platform,"VPN");
             vo.setVpnUrl(vpnVer.getUrl());
