6be1fa0edece644633ad71e22032bea3240d9367
liguoqing <liguoqing19861028@163.com>
2018-03-17 01:04:32 +0800

    111

--- src/main/java/com/timeline/vpn/service/job/reload/ZhIpCache.java
+++ src/main/java/com/timeline/vpn/service/job/reload/ZhIpCache.java
@@ -4,8 +4,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
