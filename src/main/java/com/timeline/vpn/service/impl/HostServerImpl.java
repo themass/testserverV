59e4b473a77b5deeb1828737d0eb13d0eefd7ab2
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-05-21 02:33:22 +0800

    11

--- src/main/java/com/timeline/vpn/service/impl/HostServerImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/HostServerImpl.java
@@ -213,6 +213,10 @@ public class HostServerImpl implements HostService {
         
         List<HostPo> hostList = hostV2Dao.getByLocation(location);
         boolean log = false;
+        if(Constant.VPNC.equals(baseQuery.getAppInfo().getNetType())&&Constant.LANG_ZH.equals(baseQuery.getAppInfo().getLang())) {
+            LOGGER.info("中国灯塔用户 2  1000008024"); 
+            throw new LoginException("灯塔提示：系统维护一周时间\n请使用AFree,账号通用\nQQ群：957430787\nhttp:\\\\sspacee.com 下载\n无法解析？点击分享有下载链接");
+        }
         if(location==0) {
             log = true;
            if(Constant.LANG_ZH.equals(baseQuery.getAppInfo().getLang())) {
