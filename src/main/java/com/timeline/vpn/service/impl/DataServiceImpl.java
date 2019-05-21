dd776fea4fc77cd97ac4117877f2a64093b2228b
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-05-21 09:17:30 +0800

    1

--- src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
@@ -206,7 +206,7 @@ public class DataServiceImpl implements DataService {
             desc.setDesc2(getMessage(Constant.ResultMsg.RESULT_MSG_DESC1, baseQuery.getAppInfo().getLang()));
             if(Constant.LANG_ZH.equals(baseQuery.getAppInfo().getLang())) {
                 LOGGER.info("中国灯塔用户  1000008024"); 
-                vo.setMinBuild("1000008024");
+                vo.setMinBuild("1000008022");
                 vo.setMaxBuild("1000008024"); 
                 vo.setContent("灯塔提示：系统维护一周时间\n请使用AFree,账号通用\nQQ群：957430787\nhttp:\\\\sspacee.com 下载\n无法解析？点击分享有下载链接");
                 vo.setVpnUrl("http://file.sspacee.com/file/app/FreeV9N_11818.apk");
