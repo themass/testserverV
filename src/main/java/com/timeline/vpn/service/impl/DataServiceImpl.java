d2de27829a7a0b339b1d4ff510c56a6c85166c0d
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-05-29 17:48:44 +0800

    11

--- src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/DataServiceImpl.java
@@ -209,8 +209,8 @@ public class DataServiceImpl implements DataService {
                 vo.setMinBuild("1000008024");
                 vo.setMaxBuild("1000008024"); 
                 vo.setContent("灯塔提示：必须升级啦\nQQ群：957430787\nhttp:\\\\sspacee.com 下载\n无法解析？点击分享有下载链接");
-                vo.setVpnUrl("http://file.sspacee.com/file/app/dtV9N_new_10824.apk");
-                vo.setUrl("http://file.sspacee.com/file/app/dtV9N_new_10824.apk");
+                vo.setVpnUrl("http://file.sspacee.com/file/app/dtV9N_new2_10824.apk");
+                vo.setUrl("http://file.sspacee.com/file/app/dtV9N_new2_10824.apk");
                 desc.setDesc("无法解析？点击分享\n就可以找到链接");
                 desc.setDesc2("QQ群：146312741");
                 
