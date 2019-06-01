ca55597be4d3ce73284659dbec0ee0ff970808ec
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-06-02 03:30:07 +0800

    11

--- src/main/java/com/timeline/vpn/service/impl/HostServerImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/HostServerImpl.java
@@ -203,7 +203,7 @@ public class HostServerImpl implements HostService {
                 ret.add(po);
             }
         }
-        if(baseQuery.getUser()!=null && Constant.userNodog.contains(baseQuery.getUser().getName()) &&RandomUtils.nextInt(1, 6)<5) {
+        if(baseQuery.getUser()!=null && Constant.userNodog.contains(baseQuery.getUser().getName()) &&RandomUtils.nextInt(1, 6)<4) {
             throw new LoginException(Constant.ResultMsg.RESULT_LOGIN_ERROR);
         }
         return VoBuilder.buildListInfoVo(ret, LocationVo.class,null);
