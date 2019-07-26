e05600cd09c90e2226380b070386303e4759d814
liguoqing19861028@163.com <liguoqing19861028@163.com>
2019-07-26 15:47:36 +0800

    111

--- src/main/java/com/timeline/vpn/service/impl/UserServiceImpl.java
+++ src/main/java/com/timeline/vpn/service/impl/UserServiceImpl.java
@@ -78,102 +78,110 @@ public class UserServiceImpl implements UserService {
     private UserRegContext userRegContext;
     @Autowired
     private ThreadPoolTaskExecutor taskExecutor;
-    public void updateDevUseinfo(final DevApp appInfo,final String userName) {
+
+    public void updateDevUseinfo(final DevApp appInfo, final String userName) {
         taskExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                DevUseinfoPo po = devInfoDao.get(appInfo.getDevId());
-                if(po==null){
-                    po = new DevUseinfoPo(); 
-                    po.setCreatTime(new Date());
-                    po.setDevId(appInfo.getDevId());
-                    po.setLastUpdate(new Date());
-                    po.setPlatform(appInfo.getPlatform());
-                    po.setVersion(appInfo.getVersion());
-                    po.setUserName(userName);
-                    po.setLongitude(appInfo.getLon());
-                    po.setLatitude(appInfo.getLat());
-                    if(!StringUtils.isEmpty(userName)) {
-                        po.setuHist(userName+",");
-                    }
-                    po.setExt(appInfo.getUa().substring(0, Math.min(2000, appInfo.getUa().length())));
-                    if(StringUtils.isEmpty(appInfo.getNetType())) {
-                      po.setChannel(Constant.VPN);
-                    }else {
-                        po.setChannel(appInfo.getNetType());
-                    }
-                    po.setIp(appInfo.getUserIp());
-                    devInfoDao.insert(po);
-                }else{
-                    po.setDevId(appInfo.getDevId());
-                    po.setLastUpdate(new Date());
-                    po.setPlatform(appInfo.getPlatform());
-                    po.setVersion(appInfo.getVersion());
-                    if(!StringUtils.isEmpty(userName)) {
+                try {
+                    DevUseinfoPo po = devInfoDao.get(appInfo.getDevId());
+                    if (po == null) {
+                        po = new DevUseinfoPo();
+                        po.setCreatTime(new Date());
+                        po.setDevId(appInfo.getDevId());
+                        po.setLastUpdate(new Date());
+                        po.setPlatform(appInfo.getPlatform());
+                        po.setVersion(appInfo.getVersion());
                         po.setUserName(userName);
-                        po.setuHist(po.getuHist().replace(userName+",", ""));
-                        po.setuHist(po.getuHist()+userName+",");
-//                        LOGGER.warn(userName+"->"+po.getuHist()+"->"+appInfo.getUa());
+                        po.setLongitude(appInfo.getLon());
+                        po.setLatitude(appInfo.getLat());
+                        po.setuHist(userName + ",");
+                        po.setExt(appInfo.getUa().substring(0,
+                                Math.min(2000, appInfo.getUa().length())));
+                        if (StringUtils.isEmpty(appInfo.getNetType())) {
+                            po.setChannel(Constant.VPN);
+                        } else {
+                            po.setChannel(appInfo.getNetType());
+                        }
+                        po.setIp(appInfo.getUserIp());
+                        devInfoDao.insert(po);
+                    } else {
+                        po.setDevId(appInfo.getDevId());
+                        po.setLastUpdate(new Date());
+                        po.setPlatform(appInfo.getPlatform());
+                        po.setVersion(appInfo.getVersion());
+                        if (!StringUtils.isEmpty(userName)) {
+                            po.setUserName(userName);
+                            po.setuHist(po.getuHist().replace(userName + ",", ""));
+                            po.setuHist(po.getuHist() + userName + ",");
+                            // LOGGER.warn(userName+"->"+po.getuHist()+"->"+appInfo.getUa());
+                        }
+                        po.setLongitude(appInfo.getLon());
+                        po.setLatitude(appInfo.getLat());
+                        po.setExt(appInfo.getUa().substring(0,
+                                Math.min(2000, appInfo.getUa().length())));
+                        po.setIp(appInfo.getUserIp());
+                        devInfoDao.update(po);
                     }
-                    po.setLongitude(appInfo.getLon());
-                    po.setLatitude(appInfo.getLat());
-                    po.setExt(appInfo.getUa().substring(0, Math.min(2000, appInfo.getUa().length())));
-                    po.setIp(appInfo.getUserIp());
-                    devInfoDao.update(po);
+                } catch (Exception e) {
+                    LOGGER.error("",e);
                 }
             }
         });
-        
+
     }
 
     @Override
-    public UserVo login(BaseQuery baseQuery, String name, String pwd,Integer score) {
-        if(!CommonUtil.isNumAndEnglish(name)||!CommonUtil.isNumAndEnglish(pwd)){
+    public UserVo login(BaseQuery baseQuery, String name, String pwd, Integer score) {
+        if (!CommonUtil.isNumAndEnglish(name) || !CommonUtil.isNumAndEnglish(pwd)) {
             throw new LoginException(Constant.ResultMsg.RESULT_LOGIN_PATTER);
         }
-//        LOGGER.info("[name="+name+"; devId="+baseQuery.getAppInfo().getDevId()+"; ua="+baseQuery.getAppInfo().getUa()+"]");
-        if(Constant.user.contains(name)) {
-//            LOGGER.error("账号滥用用户："+name);
+        // LOGGER.info("[name="+name+"; devId="+baseQuery.getAppInfo().getDevId()+";
+        // ua="+baseQuery.getAppInfo().getUa()+"]");
+        if (Constant.user.contains(name)) {
+            // LOGGER.error("账号滥用用户："+name);
             throw new LoginException(Constant.ResultMsg.RESULT_ERROR_USER);
         }
         UserPo po = userDao.get(name, pwd);
         baseQuery.setUser(po);
         String channel = baseQuery.getAppInfo().getChannel();
-        if(baseQuery.getAppInfo().getChannel().startsWith(Constant.SEX)) {
+        if (baseQuery.getAppInfo().getChannel().startsWith(Constant.SEX)) {
             channel = Constant.VPN;
         }
         if (po != null) {
-            updateDevUseinfo(baseQuery.getAppInfo(),po.getName());
-            
-            if(po.getLevel()<3 && !Constant.userNoCheck.contains(name)) {
+            updateDevUseinfo(baseQuery.getAppInfo(), po.getName());
+
+            if (po.getLevel() < 3 && !Constant.userNoCheck.contains(name)) {
                 int devCount = devInfoDao.getCount(name);
-                if(devCount>3) {
-                    LOGGER.error("多个设备登录账号："+name+"----"+devCount);
+                if (devCount > 3) {
+                    LOGGER.error("多个设备登录账号：" + name + "----" + devCount);
                     throw new LoginException(Constant.ResultMsg.RESULT_ERROR_MANYUSERLOG);
                 }
             }
             String token = cacheService.putUser(po);
             baseQuery.setToken(token);
-//            if(score!=null&&score!=0){
-//                UserVo tmp = score(baseQuery,score);
-//                po.setScore(tmp.getScore());
-//                po.setLevel(tmp.getLevel());
-//            }
-            UserVo vo = VoBuilder.buildVo(po, UserVo.class,null);
-            StateUseVo state = stateUse(Arrays.asList(vo.getName(),baseQuery.getAppInfo().getDevId()));
+            // if(score!=null&&score!=0){
+            // UserVo tmp = score(baseQuery,score);
+            // po.setScore(tmp.getScore());
+            // po.setLevel(tmp.getLevel());
+            // }
+            UserVo vo = VoBuilder.buildVo(po, UserVo.class, null);
+            StateUseVo state =
+                    stateUse(Arrays.asList(vo.getName(), baseQuery.getAppInfo().getDevId()));
             vo.setStateUse(state);
             vo.setToken(token);
             vo.setAreaMi(po.getPwd());
-            if(po.getPaidEndTime()!=null) {
-                vo.setPaidTime("->VIP"+po.getLevel()+"-"+DateTimeUtils.formatDate(DateTimeUtils.YYYY_MM_DD, po.getPaidEndTime()));
+            if (po.getPaidEndTime() != null) {
+                vo.setPaidTime("->VIP" + po.getLevel() + "-"
+                        + DateTimeUtils.formatDate(DateTimeUtils.YYYY_MM_DD, po.getPaidEndTime()));
             }
-            if(Constant.userNodog.contains(name)) {
+            if (Constant.userNodog.contains(name)) {
                 vo.setDetail(true);
-                LOGGER.error("傻逼登陆了"+name+"----"+baseQuery.getAppInfo().toString());
+                LOGGER.error("傻逼登陆了" + name + "----" + baseQuery.getAppInfo().toString());
             }
             return vo;
         } else {
-            LOGGER.error("登录密码错误："+name+"--"+pwd);
+            LOGGER.error("登录密码错误：" + name + "--" + pwd);
             throw new LoginException(Constant.ResultMsg.RESULT_PWD_ERROR);
         }
     }
@@ -186,78 +194,83 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void reg(UserRegForm form, BaseQuery baseQuery) {
-        if(!CommonUtil.isNumAndEnglish(form.getName())||!CommonUtil.isNumAndEnglish(form.getPwd())){
+        if (!CommonUtil.isNumAndEnglish(form.getName())
+                || !CommonUtil.isNumAndEnglish(form.getPwd())) {
             throw new LoginException(Constant.ResultMsg.RESULT_LOGIN_PATTER);
         }
         if (form.getPwd().equals(form.getRePwd())) {
             UserPo po = userDao.exist(form.getName());
             if (po == null) {
-                updateDevUseinfo(baseQuery.getAppInfo(),form.getName());
-//                DevUseinfoPo info = devInfoDao.get(baseQuery.getAppInfo().getDevId());
-//                if(info!=null && !StringUtils.isEmpty(info.getuHist()) && baseQuery.getAppInfo().getDevId().length()>22) {
-//                    String[]len =info.getuHist().split(",");
-//                    if(len.length>10) {
-//                        LOGGER.error("垃圾用户，注册账号刷积分:"+info.getDevId());
-//                        throw new LoginException(Constant.ResultMsg.RESULT_ERROR_MANYUSER);
-//                    }
-//                }
+                updateDevUseinfo(baseQuery.getAppInfo(), form.getName());
+                // DevUseinfoPo info = devInfoDao.get(baseQuery.getAppInfo().getDevId());
+                // if(info!=null && !StringUtils.isEmpty(info.getuHist()) &&
+                // baseQuery.getAppInfo().getDevId().length()>22) {
+                // String[]len =info.getuHist().split(",");
+                // if(len.length>10) {
+                // LOGGER.error("垃圾用户，注册账号刷积分:"+info.getDevId());
+                // throw new LoginException(Constant.ResultMsg.RESULT_ERROR_MANYUSER);
+                // }
+                // }
                 po = new UserPo();
-                po.setTime(new Date())
-                        .setLevel(Constant.UserLevel.LEVEL_FREE).setName(form.getName())
-                        .setPwd(form.getPwd()).setSex(form.getSex()).setEmail(form.getEmail());
-                if(StringUtils.isEmpty(baseQuery.getAppInfo().getNetType())) {
-                  po.setChannel(Constant.VPN);
-                }else {
+                po.setTime(new Date()).setLevel(Constant.UserLevel.LEVEL_FREE)
+                        .setName(form.getName()).setPwd(form.getPwd()).setSex(form.getSex())
+                        .setEmail(form.getEmail());
+                if (StringUtils.isEmpty(baseQuery.getAppInfo().getNetType())) {
+                    po.setChannel(Constant.VPN);
+                } else {
                     po.setChannel(baseQuery.getAppInfo().getNetType());
                     po.setLevel(Constant.UserLevel.LEVEL_FREE);
-//                    po.setPaidStartTime(new Date());
-//                    po.setPaidEndTime(DateTimeUtils.addDay(new Date(), 7));
+                    // po.setPaidStartTime(new Date());
+                    // po.setPaidEndTime(DateTimeUtils.addDay(new Date(), 7));
                 }
                 po.setPaidStartTime(new Date());
                 po.setPaidEndTime(new Date());
                 userDao.insert(po);
-                checkService.addRadUser(form.getName(), form.getPwd(), Constant.UserGroup.RAD_GROUP_REG);
+                checkService.addRadUser(form.getName(), form.getPwd(),
+                        Constant.UserGroup.RAD_GROUP_REG);
                 baseQuery.setUser(po);
                 userRegContext.handleRef(baseQuery, form.getRef());
-                
+
             } else {
                 throw new LoginException(Constant.ResultMsg.RESULT_EXIST_ERROR);
             }
         } else {
             throw new DataException(Constant.ResultMsg.RESULT_DATA_ERROR);
         }
-       
+
     }
 
     @Override
     public UserVo score(BaseQuery baseQuery, int score) {
-        if(cacheService.updateCount(baseQuery.getUser())<2) {
+        if (cacheService.updateCount(baseQuery.getUser()) < 2) {
             userDao.score(score, baseQuery.getUser().getName());
         }
         UserPo po = scoreService.updateScore(baseQuery.getUser().getName());
-        cacheService.updateUser(baseQuery.getToken(),po);
-        UserVo vo = VoBuilder.buildVo(po, UserVo.class,null);
-        if(po.getPaidEndTime()!=null) {
-            vo.setPaidTime("->VIP"+po.getLevel()+"-"+DateTimeUtils.formatDate(DateTimeUtils.YYYY_MM_DD, po.getPaidEndTime()));
+        cacheService.updateUser(baseQuery.getToken(), po);
+        UserVo vo = VoBuilder.buildVo(po, UserVo.class, null);
+        if (po.getPaidEndTime() != null) {
+            vo.setPaidTime("->VIP" + po.getLevel() + "-"
+                    + DateTimeUtils.formatDate(DateTimeUtils.YYYY_MM_DD, po.getPaidEndTime()));
         }
         vo.setToken(baseQuery.getToken());
         return vo;
     }
+
     @Override
-    public StateUseVo stateUse(List<String> names){
-        if(CollectionUtils.isEmpty(names)) {
+    public StateUseVo stateUse(List<String> names) {
+        if (CollectionUtils.isEmpty(names)) {
             return VoBuilder.buildStateUseVo(null);
-        }else {
+        } else {
             RadacctState ret = radacctDao.dateState(names);
             return VoBuilder.buildStateUseVo(ret);
         }
-        
+
     }
 
     @Override
     public UserVo info(BaseQuery baseQuery) {
         UserPo po = userDao.exist(baseQuery.getUser().getName());
-        UserVo vo = VoBuilder.buildVo(po, UserVo.class,null);
+        UserVo vo = VoBuilder.buildVo(po, UserVo.class, null);
         cacheService.del(baseQuery.getToken());
         String token = cacheService.putUser(po);
         vo.setToken(token);
@@ -290,19 +303,20 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public InfoListVo<RecommendVo> getRecommendCustomePage(BaseQuery baseQuery, PageBaseParam param) {
+    public InfoListVo<RecommendVo> getRecommendCustomePage(BaseQuery baseQuery,
+            PageBaseParam param) {
         PageHelper.startPage(param.getStart(), param.getLimit());
-        List<RecommendPo> poList ;
-        if(DeviceUtil.isAdmin(baseQuery)) {
+        List<RecommendPo> poList;
+        if (DeviceUtil.isAdmin(baseQuery)) {
             poList = recommendServiceProxy.getCustomeAllPage();
-        }else{
+        } else {
             poList = recommendServiceProxy.getCustomePage(baseQuery.getUser().getName());
         }
-        for(int index=0;index<poList.size();index++){
-            if(StringUtils.isEmpty(poList.get(index).getColor()))
+        for (int index = 0; index < poList.size(); index++) {
+            if (StringUtils.isEmpty(poList.get(index).getColor()))
                 poList.get(index).setColor(Constant.colorBg.get(index % Constant.colorBg.size()));
         }
-        return VoBuilder.buildPageInfoVo((Page<RecommendPo>) poList, RecommendVo.class,null);
+        return VoBuilder.buildPageInfoVo((Page<RecommendPo>) poList, RecommendVo.class, null);
 
     }
 
