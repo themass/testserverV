cfa7a9b9ff2a9600d9ba513aafa8b603fde1b6c6
liguoqing <liguoqing19861028@163.com>
2024-03-23 03:06:08 +0800

    test

--- src/main/java/com/timeline/vpn/web/common/CustomMethodHandlerAdapter.java
+++ src/main/java/com/timeline/vpn/web/common/CustomMethodHandlerAdapter.java
@@ -2,8 +2,17 @@ package com.timeline.vpn.web.common;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import com.timeline.vpn.web.common.resolver.BodyHandlerMethodArgumentResolver;
+import com.timeline.vpn.web.common.response.FastJsonHttpMessageConverterEx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.support.WebBindingInitializer;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -14,11 +23,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ServletRequestDataB
 
 public class CustomMethodHandlerAdapter extends RequestMappingHandlerAdapter {
 
-
+    @Autowired
+    private FastJsonHttpMessageConverterEx fastJsonHttpMessageConverterEx;
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CustomMethodHandlerAdapter.class);
     @Override
     protected InitBinderDataBinderFactory createDataBinderFactory(
             List<InvocableHandlerMethod> binderMethods) throws Exception {
         return new ServletRequestDataBinderFactory(binderMethods, getWebBindingInitializer());
+    }
+    @PostConstruct
+    public void initConverters() {
+        getMessageConverters().add(0,fastJsonHttpMessageConverterEx);
+        getMessageConverters().add(0,new MappingJackson2HttpMessageConverter());
+        LOGGER.info(getMessageConverters().toString());
     }
 
     class MyServletRequestDataBinderFactory extends ServletRequestDataBinderFactory {
@@ -37,9 +55,6 @@ public class CustomMethodHandlerAdapter extends RequestMappingHandlerAdapter {
             // request.getNativeRequest()));
             return binder;
         }
-
-
-
     }
 
 }
