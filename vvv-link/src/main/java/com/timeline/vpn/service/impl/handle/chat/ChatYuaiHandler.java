069c8bdd3631e3c32ab0e2a40b68623d17066e42
liguoqing <liguoqing19861028@163.com>
2024-09-05 15:28:30 +0800

    增加重试

--- vvv-link/src/main/java/com/timeline/vpn/service/impl/handle/chat/ChatYuaiHandler.java
+++ vvv-link/src/main/java/com/timeline/vpn/service/impl/handle/chat/ChatYuaiHandler.java
@@ -50,7 +50,7 @@ public class ChatYuaiHandler extends BaseChatHandle {
         List<ChatRequestMessage> chatMessages = new ArrayList<>();
         chatMessages.add(new ChatRequestSystemMessage("你是一个智能AI小助手"));
         ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions(chatMessages);
-        chatCompletionsOptions.setModel("gpt-4o");
+        chatCompletionsOptions.setModel("gpt-4o-mini");
         chatCompletionsOptions.setTopP(0.5);
         chatCompletionsOptions.setMaxTokens(2048);
         chatCompletionsOptions.setTemperature(0.2);
