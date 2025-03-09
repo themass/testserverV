497cfd76b8bc6168be54005d95a23ffa77d8bf6f
liguoqing <liguoqing19861028@163.com>
2025-03-09 11:14:07 +0800

    去掉 gpt

--- vvv-link/src/main/java/com/timeline/vpn/service/impl/handle/chat/KimiHandler.java
+++ vvv-link/src/main/java/com/timeline/vpn/service/impl/handle/chat/KimiHandler.java
@@ -29,7 +29,7 @@ public class KimiHandler extends BaseChatHandleProxy {
 
     @Override
   public boolean support(Integer t) {
-    return  t==10 || t== 6;
+    return  false;
   }
     public Choice chatWithGpt(BaseQuery baseQuery, String prompt) throws Exception {
 
